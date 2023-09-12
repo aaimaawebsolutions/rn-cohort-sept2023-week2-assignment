129b1402235aa5191d688cb318220a4a331888c1
AYUSH KUMAR <user@AKs-MacBook-Air.local>
2023-09-13 02:33:36 +0530

    Stack-Navigation/Drawer Navigator

--- App.js
+++ App.js
@@ -1,23 +1,26 @@
 import * as React from "react";
 import { View, Text } from "react-native";
+import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import HomeScreen from "./components/HomeScreen";
 import AboutScreen from "./components/AboutScreen"; // Correct import
 import ContactScreen from "./components/ContactScreen";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator initialRouteName="Home">
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="About" component={AboutScreen} />
-        <Tab.Screen name="Contact" component={ContactScreen} />
-      </Tab.Navigator>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="About" component={AboutScreen} />
+        <Drawer.Screen name="Contact" component={ContactScreen} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
