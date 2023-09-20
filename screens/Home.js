35ad56f85d456a21f410dda45fad86adecf510e1
Adesh000 <adeshnayak6@gmail.com>
2023-09-20 13:41:30 +0530

    card added

--- screens/Home.js
+++ screens/Home.js
@@ -1,23 +1,31 @@
 import { StyleSheet, View } from "react-native";
 import React from "react";
-import { Button, Text } from "react-native-paper";
+import { Button, Card, Text } from "react-native-paper";
 
 const Home = () => {
   return (
-    <View style={styles.mainContainer}>
-      <Button mode="outlined">Custom Button</Button>
-      <Button mode="contained">Custom Button</Button>
-      <Button mode="contained-tonal">Custom Button</Button>
-      <Button mode="elevated">Custom Button</Button>
+    <>
+      <View style={styles.mainContainer}>
+        <Button mode="contained-tonal">Custom Button</Button>
+        <Button mode="elevated">Custom Button</Button>
 
-      <Text
-        variant="displayMedium"
-        style={{ color: "blue" }}
-        theme={{ colors: { primary: "green" } }}
-      >
-        Adesh Nayak
-      </Text>
-    </View>
+        <Text
+          variant="displayMedium"
+          style={{ color: "blue" }}
+          theme={{ colors: { primary: "green" } }}
+        >
+          Adesh Nayak
+        </Text>
+      </View>
+      <Card mode="elevated">
+        <Card.Title title="Card" subtitle="Elevated Card" />
+        <Card.Cover source={{ uri: "https://picsum.photos/700" }} />
+        <Card.Actions>
+          <Button mode="outlined">Cancel</Button>
+          <Button mode="contained">Ok</Button>
+        </Card.Actions>
+      </Card>
+    </>
   );
 };
 
@@ -25,7 +33,6 @@ export default Home;
 
 const styles = StyleSheet.create({
   mainContainer: {
-    flex: 1,
     alignItems: "center",
     justifyContent: "center",
   },
